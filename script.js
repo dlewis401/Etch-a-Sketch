b8fd4585b7186cd5c806645d7af02982398bd8d9
lewisandlloydgaming@gmail.com <lewisandlloydgaming@gmail.com>
2021-08-30 04:39:48 +0100

    Rewrote the whole code - adding a grid change function and overall making it better

--- script.js
+++ script.js
@@ -1,53 +1,56 @@
-let grid = document.querySelector(".Grid");
-
-let black_and_white = ["white", "black"];
-
-function Grid_16x16() {
-
-    let i = 0; 
-
-while (i < 256) {
-    let gridDivs = document.createElement('div');
-    gridDivs.style.backgroundColor = "black";
-    gridDivs.style.width = "2em";
-    gridDivs.classList.add("gridDiv")
-    gridDivs.style.height = "2em";
-    grid.appendChild(gridDivs);
-    i++;
-}
-
-}
-
-function change_grid_size() {
-    let grid_button = document.querySelector('#grid_size');
-    let gridDivs = document.querySelectorAll('div.gridDiv');
-
-    grid_button.addEventListener('click', () => {
-    let userPrompt = prompt("Enter a new value for the Grid! You MUST enter a number between 0 to 100.");
+let container = document.querySelector('.container');
+container.style.gridTemplateColumns = "repeat(16, 2em)";
+container.style.justifyContent = "center";
+container.style.gridGap = "0.1em";
+let change_grid_button = document.querySelector('.change_grid');
 
 
-/* Prompt */
-i = 0
+// This code creates the 16x16 grid.
 
-if (userPrompt > 100) {
-    alert("Invalid.")
-}
+function grid_creation(i, grid_number) {
+    while (i < grid_number) {
+        let create_div = document.createElement('div');
+        create_div.classList = "Grid_Div"
+        create_div.style.width = "2em";
+        create_div.style.height = "2em";
+        create_div.style.backgroundColor = "black";
+        container.appendChild(create_div);
+        i++;
+    };
+}; 
 
-})};
+function changeColourDefault(colours) {
+    let grid_div_all = document.querySelectorAll('div.Grid_Div');
+    grid_div_all.forEach((Grid_Div) => {
+        Grid_Div.addEventListener('mouseover', () => {
+        Grid_Div.style.backgroundColor = colours[Math.floor(Math.random() * colours.length)];
+        })
+    })};
 
 
-function change_grid_colour() {
-let grid_board = grid.querySelectorAll('div.gridDiv');
-
-grid_board.forEach( (gridDiv) => {
-    gridDiv.addEventListener('mouseover', () => {
-        gridDiv.style.backgroundColor = black_and_white[Math.floor(Math.random() * black_and_white.length)];
-    });
-});
 
 
-};
+function change_grid() {
+    let grid_div = document.querySelectorAll('.Grid_Div');
+    change_grid_button.addEventListener('click', () => {
+        let grid_number = parseInt(prompt('Enter a Grid Size Maximum of 64'));
 
-Grid_16x16();
-change_grid_colour();
-change_grid_size();
+        function wipe_grid() {
+        
+        
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }};
+
+        if (grid_number <= 64) {
+            wipe_grid();
+            grid_creation(0, grid_number);
+            changeColourDefault(["black", "white"])
+
+            }
+    })};
+
+
+grid_creation(0, 256);
+change_grid()
+changeColourDefault(["black", "white"])
